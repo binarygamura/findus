59d87c91ff6dda62d321d725dc03dafe4b690a96
tierhilfe <daniektj@RD-50058840-N.ads.mobilcom.de>
2017-03-14 15:38:08 +0100

    Help

--- js/module/admissionType.js
+++ js/module/admissionType.js
@@ -31,6 +31,7 @@ $(document).ready(function () {
             var admissionTypeId = selectedAdmissionType.id;
             var admissionTypeName = selectedAdmissionType.name;
             var admissionTypeDescription = selectedAdmissionType.description;
+            ////TODO: FRED! bitte . danke...
             $.get("./templates/admission/add_admissionType.htpl", function (data) {
                 var content = $(data).dialog({
                     title: "Eingangsart \""+selectedAdmissionType.name+"\" bearbeiten",
